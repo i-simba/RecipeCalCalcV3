ff5581ec2f8e648520483e2fd02da2abbbddff1e
i-simba <ivasimbulan@gmail.com>
2023-04-13 04:17:20 -0500

    Implemented adding clicked ingredients to panel.

--- RecipeCalCalcV3/ChildForms/BuilderForm.Designer.cs
+++ RecipeCalCalcV3/ChildForms/BuilderForm.Designer.cs
@@ -32,50 +32,181 @@
             this.liquidPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.veggiePanel = new System.Windows.Forms.FlowLayoutPanel();
             this.proteinPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mainIngredientPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // miscPanel
             // 
             this.miscPanel.AutoScroll = true;
             this.miscPanel.BackColor = System.Drawing.Color.Silver;
-            this.miscPanel.Location = new System.Drawing.Point(830, 640);
+            this.miscPanel.Location = new System.Drawing.Point(830, 690);
             this.miscPanel.Name = "miscPanel";
-            this.miscPanel.Size = new System.Drawing.Size(310, 200);
+            this.miscPanel.Size = new System.Drawing.Size(310, 150);
             this.miscPanel.TabIndex = 7;
             // 
             // liquidPanel
             // 
             this.liquidPanel.AutoScroll = true;
             this.liquidPanel.BackColor = System.Drawing.Color.Silver;
-            this.liquidPanel.Location = new System.Drawing.Point(830, 430);
+            this.liquidPanel.Location = new System.Drawing.Point(830, 480);
             this.liquidPanel.Name = "liquidPanel";
-            this.liquidPanel.Size = new System.Drawing.Size(310, 200);
+            this.liquidPanel.Size = new System.Drawing.Size(310, 150);
             this.liquidPanel.TabIndex = 6;
             // 
             // veggiePanel
             // 
             this.veggiePanel.AutoScroll = true;
             this.veggiePanel.BackColor = System.Drawing.Color.Silver;
-            this.veggiePanel.Location = new System.Drawing.Point(830, 220);
+            this.veggiePanel.Location = new System.Drawing.Point(830, 270);
             this.veggiePanel.Name = "veggiePanel";
-            this.veggiePanel.Size = new System.Drawing.Size(310, 200);
+            this.veggiePanel.Size = new System.Drawing.Size(310, 150);
             this.veggiePanel.TabIndex = 5;
             // 
             // proteinPanel
             // 
             this.proteinPanel.AutoScroll = true;
             this.proteinPanel.BackColor = System.Drawing.Color.Silver;
-            this.proteinPanel.Location = new System.Drawing.Point(830, 10);
+            this.proteinPanel.Location = new System.Drawing.Point(830, 60);
             this.proteinPanel.Name = "proteinPanel";
-            this.proteinPanel.Size = new System.Drawing.Size(310, 200);
+            this.proteinPanel.Size = new System.Drawing.Size(310, 150);
             this.proteinPanel.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(130)))), ((int)(((byte)(126)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(830, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 50);
+            this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PROTEIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(130)))), ((int)(((byte)(126)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(830, 215);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(310, 50);
+            this.panel2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "VEGGIE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(130)))), ((int)(((byte)(126)))));
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(830, 425);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(310, 50);
+            this.panel3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 50);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "LIQUIDS";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(130)))), ((int)(((byte)(126)))));
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(830, 635);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(310, 50);
+            this.panel4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(310, 50);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "MISC";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainIngredientPanel
+            // 
+            this.mainIngredientPanel.AutoScroll = true;
+            this.mainIngredientPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(221)))), ((int)(((byte)(217)))));
+            this.mainIngredientPanel.Location = new System.Drawing.Point(10, 5);
+            this.mainIngredientPanel.Name = "mainIngredientPanel";
+            this.mainIngredientPanel.Size = new System.Drawing.Size(810, 625);
+            this.mainIngredientPanel.TabIndex = 12;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(163)))), ((int)(((byte)(157)))));
+            this.panel5.Location = new System.Drawing.Point(10, 635);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(810, 50);
+            this.panel5.TabIndex = 13;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(130)))), ((int)(((byte)(126)))));
+            this.panel6.Location = new System.Drawing.Point(10, 690);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(810, 150);
+            this.panel6.TabIndex = 14;
             // 
             // BuilderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(221)))), ((int)(((byte)(217)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(241)))), ((int)(((byte)(237)))));
             this.ClientSize = new System.Drawing.Size(1150, 850);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.mainIngredientPanel);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.miscPanel);
             this.Controls.Add(this.liquidPanel);
             this.Controls.Add(this.veggiePanel);
@@ -83,6 +214,10 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "BuilderForm";
             this.Text = "BuilderForm";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,5 +228,16 @@
         private System.Windows.Forms.FlowLayoutPanel liquidPanel;
         private System.Windows.Forms.FlowLayoutPanel veggiePanel;
         private System.Windows.Forms.FlowLayoutPanel proteinPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel mainIngredientPanel;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
     }
 }
--- RecipeCalCalcV3/ChildForms/BuilderForm.cs
+++ RecipeCalCalcV3/ChildForms/BuilderForm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +17,20 @@ namespace RecipeCalCalcV3.ChildForms
         // MainFrom object
         MainForm main = null;
 
+        // Button List & Boolean List
+        private List<Button> ingredientButtons = null;
+        private List<Boolean> isAdded = null;
+
         public BuilderForm(MainForm m)
         {
             InitializeComponent();
 
             // Setting 'main' to passed in MainForm 'm'.
             main = m;
-            
+
+            // Initialize lists.
+            ingredientButtons = new List<Button>();
+            isAdded = new List<Boolean>();
             initButtons();
         }
 
@@ -36,9 +44,13 @@ namespace RecipeCalCalcV3.ChildForms
             foreach (Ingredient ing in Program.ingredients)
             {
                 Button temp = new Button();
+                temp.Name = ing.getName();
+                temp.Image = ing.getImage();
                 temp.Width = 64;
                 temp.Height = 64;
-                temp.Image = ing.getImage();
+                temp.Click += new EventHandler(button_Click);
+
+                Boolean b = false;
 
                 ToolTip tip = new ToolTip();
                 tip.SetToolTip(temp, ing.getTipName());
@@ -59,7 +71,45 @@ namespace RecipeCalCalcV3.ChildForms
                 {
                     miscPanel.Controls.Add(temp);
                 }
+                ingredientButtons.Add(temp);
+                isAdded.Add(b);
             }
-        }        
+        }
+
+        /**
+         * 
+         */
+        private void button_Click(object sender, EventArgs e)
+        {
+            Panel container = new Panel();
+            PictureBox pic = new PictureBox();
+
+            container.BackColor = Color.FromArgb(168, 163, 157);
+            container.Padding = new Padding(5, 5, 0, 0);
+            container.Width = 385;
+            container.Height = 70;
+
+            pic.BackColor = Color.FromArgb(134, 130, 126);
+            pic.SetBounds(3, 3, 64, 64);
+            pic.Width = 64;
+            pic.Height = 64;
+
+            int i = 0;
+            foreach (Button button in ingredientButtons)
+            {
+                if (sender == button && isAdded[i] == false)
+                {
+                    pic.Image = button.Image;
+                    isAdded[i] = true;
+
+                    container.Controls.Add(pic);
+                    mainIngredientPanel.Controls.Add(container);
+
+                    i++;
+                }
+                else i++;
+            }
+        }
+
     }
 }
