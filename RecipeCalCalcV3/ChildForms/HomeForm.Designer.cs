60a28986a5b57cd3f5233998cc7b5097c1f213fc
i-simba <ivasimbulan@gmail.com>
2023-05-03 18:13:20 -0500

    Implemented ingredient image animations.

--- RecipeCalCalcV3/ChildForms/HomeForm.Designer.cs
+++ RecipeCalCalcV3/ChildForms/HomeForm.Designer.cs
@@ -28,30 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ratPanel = new System.Windows.Forms.Panel();
-            this.ratPicBox1 = new System.Windows.Forms.PictureBox();
-            this.ratPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ratPicBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.ingDisplayPanel = new System.Windows.Forms.Panel();
+            this.ingPicsAnim = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // ratPanel
+            // ingDisplayPanel
             // 
-            this.ratPanel.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ratPanel.Controls.Add(this.ratPicBox1);
-            this.ratPanel.Location = new System.Drawing.Point(630, 10);
-            this.ratPanel.Name = "ratPanel";
-            this.ratPanel.Size = new System.Drawing.Size(510, 510);
-            this.ratPanel.TabIndex = 0;
+            this.ingDisplayPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ingDisplayPanel.Location = new System.Drawing.Point(45, 50);
+            this.ingDisplayPanel.Name = "ingDisplayPanel";
+            this.ingDisplayPanel.Size = new System.Drawing.Size(950, 74);
+            this.ingDisplayPanel.TabIndex = 0;
             // 
-            // ratPicBox1
+            // ingPicsAnim
             // 
-            this.ratPicBox1.Location = new System.Drawing.Point(0, 0);
-            this.ratPicBox1.Name = "ratPicBox1";
-            this.ratPicBox1.Size = new System.Drawing.Size(510, 510);
-            this.ratPicBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.ratPicBox1.TabIndex = 0;
-            this.ratPicBox1.TabStop = false;
-            this.ratPicBox1.Click += new System.EventHandler(this.ratPicBox1_Click);
+            this.ingPicsAnim.Enabled = true;
+            this.ingPicsAnim.Interval = 30;
+            this.ingPicsAnim.Tick += new System.EventHandler(this.ingPicsAnim_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(92)))), ((int)(((byte)(69)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(1005, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD INGREDIENT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // HomeForm
             // 
@@ -59,19 +68,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(221)))), ((int)(((byte)(217)))));
             this.ClientSize = new System.Drawing.Size(1150, 850);
-            this.Controls.Add(this.ratPanel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ingDisplayPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HomeForm";
             this.Text = "HomeForm";
-            this.ratPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.ratPicBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel ratPanel;
-        private System.Windows.Forms.PictureBox ratPicBox1;
+        private System.Windows.Forms.Panel ingDisplayPanel;
+        private System.Windows.Forms.Timer ingPicsAnim;
+        private System.Windows.Forms.Button button1;
     }
 }
